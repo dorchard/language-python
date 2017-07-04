308148476b8cfc6c29723e7a4ec54069fe69a225
Maxime Fischer <maxime2fischer@gmail.com>
2017-07-04 11:07:37 +0200

    Add OVERLAPPABLE pragma to pretty instances.

--- src/Language/Python/Common/PrettyAST.hs
+++ src/Language/Python/Common/PrettyAST.hs
@@ -1,19 +1,19 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      : Language.Python.Version2.Syntax.PrettyAST
--- Copyright   : (c) 2009 Bernie Pope 
+-- Copyright   : (c) 2009 Bernie Pope
 -- License     : BSD-style
 -- Maintainer  : bjpop@csse.unimelb.edu.au
 -- Stability   : experimental
 -- Portability : ghc
 --
--- Pretty printing of the Python abstract syntax (version 2.x and 3.x). 
+-- Pretty printing of the Python abstract syntax (version 2.x and 3.x).
 -----------------------------------------------------------------------------
 
 module Language.Python.Common.PrettyAST () where
 
 import Language.Python.Common.Pretty
-import Language.Python.Common.AST 
+import Language.Python.Common.AST
 
 --------------------------------------------------------------------------------
 
@@ -32,10 +32,10 @@ prettyString :: String -> Doc
 -- prettyString str = text (show str)
 prettyString str = text str
 
-instance Pretty (Module a) where
-   pretty (Module stmts) = vcat $ map pretty stmts 
+instance {-# OVERLAPPABLE #-} Pretty (Module a) where
+   pretty (Module stmts) = vcat $ map pretty stmts
 
-instance Pretty (Ident a) where
+instance {-# OVERLAPPABLE #-} Pretty (Ident a) where
    pretty name@(Ident {}) = text $ ident_string name
 
 prettyDottedName :: DottedName a -> Doc
@@ -44,36 +44,36 @@ prettyDottedName [name] = pretty name
 prettyDottedName (name:rest@(_:_))
    = pretty name <> dot <> prettyDottedName rest
 
-instance Pretty (ImportItem a) where
+instance {-# OVERLAPPABLE #-} Pretty (ImportItem a) where
    pretty (ImportItem {import_item_name = name, import_as_name = asName})
       = prettyDottedName name <+> (maybe empty (\n -> text "as" <+> pretty n) asName)
 
-instance Pretty (FromItem a) where
+instance {-# OVERLAPPABLE #-} Pretty (FromItem a) where
    pretty (FromItem { from_item_name = name, from_as_name = asName })
-      = pretty name <+> (maybe empty (\n -> text "as" <+> pretty n) asName) 
+      = pretty name <+> (maybe empty (\n -> text "as" <+> pretty n) asName)
 
-instance Pretty (FromItems a) where
+instance {-# OVERLAPPABLE #-} Pretty (FromItems a) where
    pretty ImportEverything {} = char '*'
-   pretty (FromItems { from_items_items = [item] }) = pretty item 
+   pretty (FromItems { from_items_items = [item] }) = pretty item
    pretty (FromItems { from_items_items = items }) = parens (commaList items)
 
-instance Pretty (ImportRelative a) where
-   pretty (ImportRelative { import_relative_dots = dots, import_relative_module = mod }) 
+instance {-# OVERLAPPABLE #-} Pretty (ImportRelative a) where
+   pretty (ImportRelative { import_relative_dots = dots, import_relative_module = mod })
       = case mod of
-           Nothing -> dotDoc 
-           Just name -> dotDoc <> prettyDottedName name 
+           Nothing -> dotDoc
+           Just name -> dotDoc <> prettyDottedName name
       where
       dotDoc = text (replicate dots '.')
 
 prettySuite :: [Statement a] -> Doc
-prettySuite stmts = vcat $ map pretty stmts 
+prettySuite stmts = vcat $ map pretty stmts
 
 optionalKeywordSuite :: String -> [Statement a] -> Doc
 optionalKeywordSuite _ [] = empty
 optionalKeywordSuite keyword stmts = text keyword <> colon $+$ indent (prettySuite stmts)
 
 prettyParenList :: Pretty a => [a] -> Doc
-prettyParenList = parens . commaList 
+prettyParenList = parens . commaList
 
 prettyOptionalList :: Pretty a => [a] -> Doc
 prettyOptionalList [] = empty
@@ -85,9 +85,9 @@ prettyGuards ((cond,body):guards)
    = text "elif" <+> pretty cond <> colon $+$ indent (prettySuite body) $+$
      prettyGuards guards
 
-instance Pretty (Statement a) where
-   -- pretty :: Statement -> Doc 
-   pretty (Import { import_items = items}) = text "import" <+> commaList items 
+instance {-# OVERLAPPABLE #-} Pretty (Statement a) where
+   -- pretty :: Statement -> Doc
+   pretty (Import { import_items = items}) = text "import" <+> commaList items
    pretty stmt@(FromImport {})
       = text "from" <+> pretty (from_module stmt) <+> text "import" <+> pretty (from_items stmt)
    pretty stmt@(While {})
@@ -97,16 +97,16 @@ instance Pretty (Statement a) where
       = text "for" <+> commaList (for_targets stmt) <+> text "in" <+> pretty (for_generator stmt) <> colon $+$
         indent (prettySuite (for_body stmt)) $+$ optionalKeywordSuite "else" (for_else stmt)
    pretty stmt@(Fun {})
-      = text "def" <+> pretty (fun_name stmt) <> parens (commaList (fun_args stmt)) <+> 
+      = text "def" <+> pretty (fun_name stmt) <> parens (commaList (fun_args stmt)) <+>
         perhaps (fun_result_annotation stmt) (text "->") <+>
-        pretty (fun_result_annotation stmt) <> colon $+$ indent (prettySuite (fun_body stmt)) 
+        pretty (fun_result_annotation stmt) <> colon $+$ indent (prettySuite (fun_body stmt))
    pretty stmt@(Class {})
-      = text "class" <+> pretty (class_name stmt) <> prettyOptionalList (class_args stmt) <> 
-        colon $+$ indent (prettySuite (class_body stmt)) 
+      = text "class" <+> pretty (class_name stmt) <> prettyOptionalList (class_args stmt) <>
+        colon $+$ indent (prettySuite (class_body stmt))
    pretty stmt@(Conditional { cond_guards = guards, cond_else = optionalElse })
       = case guards of
-           (cond,body):xs -> 
-              text "if" <+> pretty cond <> colon $+$ indent (prettySuite body) $+$ 
+           (cond,body):xs ->
+              text "if" <+> pretty cond <> colon $+$ indent (prettySuite body) $+$
               prettyGuards xs $+$
               optionalKeywordSuite "else" optionalElse
            [] -> error "Attempt to pretty print conditional statement with empty guards"
@@ -114,14 +114,14 @@ instance Pretty (Statement a) where
    pretty (Assign { assign_to = pattern, assign_expr = e })
       = equalsList pattern <+> equals <+> pretty e
    pretty (AugmentedAssign { aug_assign_to = to_expr, aug_assign_op = op, aug_assign_expr = e})
-      = pretty to_expr <+> pretty op <+> pretty e 
+      = pretty to_expr <+> pretty op <+> pretty e
    pretty (Decorated { decorated_decorators = decs, decorated_def = stmt})
       = vcat (map pretty decs) $+$ pretty stmt
    pretty (Return { return_expr = e }) = text "return" <+> pretty e
    pretty (Try { try_body = body, try_excepts = handlers, try_else = optionalElse, try_finally = finally})
       = text "try" <> colon $+$ indent (prettySuite body) $+$
-        prettyHandlers handlers $+$ optionalKeywordSuite "else" optionalElse $+$ 
-        optionalKeywordSuite "finally" finally 
+        prettyHandlers handlers $+$ optionalKeywordSuite "else" optionalElse $+$
+        optionalKeywordSuite "finally" finally
    pretty (Raise { raise_expr = e })
       = text "raise" <+> pretty e
    pretty (With { with_context = context, with_body = body })
@@ -139,8 +139,8 @@ instance Pretty (Statement a) where
       text "print" <> (if have_chevron then text " >>" else empty) <+>
       hcat (punctuate comma (map pretty es)) <>
       if trail_comma then comma else empty
-   pretty (Exec { exec_expr = e, exec_globals_locals = gls }) = 
-      text "exec" <+> pretty e <+> 
+   pretty (Exec { exec_expr = e, exec_globals_locals = gls }) =
+      text "exec" <+> pretty e <+>
       maybe empty (\ (globals, next) -> text "in" <+> pretty globals <+>
       maybe empty (\locals -> comma <+> pretty locals) next) gls
 
@@ -152,76 +152,76 @@ prettyHandlers :: [Handler a] -> Doc
 prettyHandlers = foldr (\next rec -> pretty next $+$ rec) empty
 
 
-instance Pretty (Handler a) where
+instance {-# OVERLAPPABLE #-} Pretty (Handler a) where
    pretty (Handler { handler_clause = exceptClause, handler_suite = suite })
       = pretty exceptClause <> colon $+$ indent (prettySuite suite)
 
-instance Pretty (ExceptClause a) where
+instance {-# OVERLAPPABLE #-} Pretty (ExceptClause a) where
    pretty (ExceptClause { except_clause = Nothing }) = text "except"
-   pretty (ExceptClause { except_clause = Just (e, target)}) 
+   pretty (ExceptClause { except_clause = Just (e, target)})
       = text "except" <+> pretty e <+> maybe empty (\t -> text "as" <+> pretty t) target
 
-instance Pretty (RaiseExpr a) where
-   pretty (RaiseV3 e) = 
+instance {-# OVERLAPPABLE #-} Pretty (RaiseExpr a) where
+   pretty (RaiseV3 e) =
       maybe empty (\ (x, fromE) -> pretty x <+> (maybe empty (\f -> text "from" <+> pretty f) fromE)) e
-   pretty (RaiseV2 exp) = 
-      maybe empty (\ (e1, next1) -> pretty e1 <> 
-      maybe empty (\ (e2, next2) -> comma <+> pretty e2 <> 
+   pretty (RaiseV2 exp) =
+      maybe empty (\ (e1, next1) -> pretty e1 <>
+      maybe empty (\ (e2, next2) -> comma <+> pretty e2 <>
       maybe empty (\ e3 -> comma <+> pretty e3) next2) next1) exp
 
-instance Pretty (Decorator a) where
+instance {-# OVERLAPPABLE #-} Pretty (Decorator a) where
    pretty (Decorator { decorator_name = name, decorator_args = args })
       = char '@' <> prettyDottedName name <+> prettyOptionalList args
 
-instance Pretty (Parameter a) where
+instance {-# OVERLAPPABLE #-} Pretty (Parameter a) where
    pretty (Param { param_name = ident, param_py_annotation = annot, param_default = def })
-      = pretty ident <> (maybe empty (\e -> colon <> pretty e <> space) annot) <> 
-        maybe empty (\e -> equals <> pretty e) def 
+      = pretty ident <> (maybe empty (\e -> colon <> pretty e <> space) annot) <>
+        maybe empty (\e -> equals <> pretty e) def
    pretty (VarArgsPos { param_name = ident, param_py_annotation = annot})
       = char '*' <> pretty ident <> (maybe empty (\e -> colon <> pretty e) annot)
    pretty (VarArgsKeyword { param_name = ident, param_py_annotation = annot })
       = text "**" <> pretty ident <> (maybe empty (\e -> colon <> pretty e) annot)
-   pretty EndPositional {} = char '*' 
+   pretty EndPositional {} = char '*'
    pretty (UnPackTuple { param_unpack_tuple = tuple, param_default = def })
       = pretty tuple <> maybe empty (\e -> equals <> pretty e) def
 
-instance Pretty (ParamTuple a) where
+instance {-# OVERLAPPABLE #-} Pretty (ParamTuple a) where
    pretty (ParamTupleName { param_tuple_name = name }) = pretty name
    pretty (ParamTuple { param_tuple = tuple }) = prettyParenList tuple
 
-instance Pretty (Argument a) where
+instance {-# OVERLAPPABLE #-} Pretty (Argument a) where
    pretty (ArgExpr { arg_expr = e }) = pretty e
    pretty (ArgVarArgsPos { arg_expr = e}) = char '*' <> pretty e
    pretty (ArgVarArgsKeyword { arg_expr = e }) = text "**" <> pretty e
-   pretty (ArgKeyword { arg_keyword = ident, arg_expr = e }) 
+   pretty (ArgKeyword { arg_keyword = ident, arg_expr = e })
       = pretty ident <> equals <> pretty e
 
-instance Pretty (Comprehension a) where
-   pretty (Comprehension { comprehension_expr = e, comprehension_for = for }) 
-      = pretty e <+> pretty for 
+instance {-# OVERLAPPABLE #-} Pretty (Comprehension a) where
+   pretty (Comprehension { comprehension_expr = e, comprehension_for = for })
+      = pretty e <+> pretty for
 
-instance Pretty (ComprehensionExpr a) where
+instance {-# OVERLAPPABLE #-} Pretty (ComprehensionExpr a) where
    pretty (ComprehensionExpr e) = pretty e
    pretty (ComprehensionDict d) = pretty d
 
-instance Pretty (CompFor a) where
-   pretty (CompFor { comp_for_exprs = es, comp_in_expr = e, comp_for_iter = iter }) 
+instance {-# OVERLAPPABLE #-} Pretty (CompFor a) where
+   pretty (CompFor { comp_for_exprs = es, comp_in_expr = e, comp_for_iter = iter })
       = text "for" <+> commaList es <+> text "in" <+> pretty e <+> pretty iter
 
-instance Pretty (CompIf a) where
-   pretty (CompIf { comp_if = e, comp_if_iter = iter }) 
-      = text "if" <+> pretty e <+> pretty iter 
+instance {-# OVERLAPPABLE #-} Pretty (CompIf a) where
+   pretty (CompIf { comp_if = e, comp_if_iter = iter })
+      = text "if" <+> pretty e <+> pretty iter
 
-instance Pretty (CompIter a) where
-   pretty (IterFor { comp_iter_for = compFor }) = pretty compFor 
+instance {-# OVERLAPPABLE #-} Pretty (CompIter a) where
+   pretty (IterFor { comp_iter_for = compFor }) = pretty compFor
    pretty (IterIf { comp_iter_if = compIf }) = pretty compIf
 
-instance Pretty (Expr a) where
+instance {-# OVERLAPPABLE #-} Pretty (Expr a) where
    pretty (Var { var_ident = i }) = pretty i
-   pretty (Int { expr_literal = str }) = text str 
-   pretty (LongInt { expr_literal = str }) = text str 
-   pretty (Float { expr_literal = str }) = text str 
-   pretty (Imaginary { expr_literal = str }) = text str 
+   pretty (Int { expr_literal = str }) = text str
+   pretty (LongInt { expr_literal = str }) = text str
+   pretty (Float { expr_literal = str }) = text str
+   pretty (Imaginary { expr_literal = str }) = text str
    pretty (Bool { bool_value = b}) = pretty b
    pretty None {} = text "None"
    pretty Ellipsis {} = text "..."
@@ -232,14 +232,14 @@ instance Pretty (Expr a) where
    pretty (Subscript { subscriptee = e, subscript_expr = sub })
       = pretty e <> brackets (pretty sub)
    pretty (SlicedExpr { slicee = e, slices = ss })
-      = pretty e <> brackets (commaList ss) 
+      = pretty e <> brackets (commaList ss)
    pretty (CondExpr { ce_true_branch = trueBranch, ce_condition = cond, ce_false_branch = falseBranch })
       = pretty trueBranch <+> text "if" <+> pretty cond <+> text "else" <+> pretty falseBranch
    pretty (BinaryOp { operator = op, left_op_arg = left, right_op_arg = right })
       = pretty left <+> pretty op <+> pretty right
    pretty (UnaryOp { operator = op, op_arg = e }) = pretty op <+> pretty e
    pretty (Dot { dot_expr = e, dot_attribute = a }) =
-      pretty e <> dot <> pretty a 
+      pretty e <> dot <> pretty a
    pretty (Lambda { lambda_args = args, lambda_body = body })
       = text "lambda" <+> commaList args <> colon <+> pretty body
    pretty (Tuple { tuple_exprs = es }) =
@@ -248,7 +248,7 @@ instance Pretty (Expr a) where
          [e] -> pretty e <> comma
          _ -> commaList es
    pretty (Yield { yield_arg = arg })
-      = text "yield" <+> pretty arg 
+      = text "yield" <+> pretty arg
    pretty (Generator { gen_comprehension = gc }) = parens $ pretty gc
    pretty (ListComp { list_comprehension = lc }) = brackets $ pretty lc
    pretty (List { list_exprs = es }) = brackets (commaList es)
@@ -261,20 +261,20 @@ instance Pretty (Expr a) where
    pretty (StringConversion { backquoted_expr = e }) = char '`' <> pretty e <> char '`'
    pretty (Starred { starred_expr = e }) = char '*' <> pretty e
 
-instance Pretty (YieldArg a) where
+instance {-# OVERLAPPABLE #-} Pretty (YieldArg a) where
    pretty (YieldFrom e _annot) = text "from" <+> pretty e
    pretty (YieldExpr e) = pretty e
 
-instance Pretty (DictMappingPair a) where
+instance {-# OVERLAPPABLE #-} Pretty (DictMappingPair a) where
    pretty (DictMappingPair key val) = pretty key <> colon <+> pretty val
 
-instance Pretty (Slice a) where
+instance {-# OVERLAPPABLE #-} Pretty (Slice a) where
    pretty (SliceProper { slice_lower = lower, slice_upper = upper, slice_stride = stride })
       = pretty lower <> colon <> pretty upper <> (maybe empty (\s -> colon <> pretty s) stride)
    pretty (SliceExpr { slice_expr = e }) = pretty e
    pretty (SliceEllipsis {}) = text "..."
 
-instance Pretty (Op a) where
+instance {-# OVERLAPPABLE #-} Pretty (Op a) where
    pretty (And {}) = text "and"
    pretty (Or {}) = text "or"
    pretty (Not {}) = text "not"
@@ -303,7 +303,7 @@ instance Pretty (Op a) where
    pretty (Invert {}) = text "~"
    pretty (Modulo {}) = text "%"
 
-instance Pretty (AssignOp a) where
+instance {-# OVERLAPPABLE #-} Pretty (AssignOp a) where
    pretty (PlusAssign {}) = text "+="
    pretty (MinusAssign {}) = text "-="
    pretty (MultAssign {}) = text "*="
